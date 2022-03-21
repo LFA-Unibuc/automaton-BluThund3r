8c1c903683c928efc4b1059ff6f75041e512cd43
George Radu <bluthundr3@gmail.com>
2022-03-21 09:28:49 +0200

    Done

--- automaton.py
+++ automaton.py
@@ -10,7 +10,66 @@ class Automaton():
         Returns true if the config file is valid,
         and raises a ValidationException if the config is invalid.
         """
-        return "test here"
+        with open("input.txt") as fin:
+            lines = [line for line in fin if not line.startswith('#') and line != '\n']
+
+        start_state = ''
+        final_states = []
+        states = []
+        transitions = []
+        words = []
+        isStates = False
+        isTransitions = False
+        isWords = False
+        no_start = 0
+
+        for line in lines:
+            if line.strip().startswith('End'):
+                if isWords:
+                    isWords = False
+                elif isStates:
+                    isStates = False
+                elif isTransitions:
+                    isTransitions = False
+            elif line.strip().startswith("Sigma"):
+                isWords = True
+            elif line.strip().startswith("Transitions"):
+                isTransitions = True
+            elif line.strip().startswith("States"):
+                isStates = True
+            else:
+                if isStates:
+                    temp = [el.strip() for el in line.split(',')]
+                    state = temp[0]
+                    type1 = type2 = ''
+                    if len(temp) == 2:
+                        type1 = temp[1]
+                    elif len(temp) == 3:
+                        type1 = temp[1]
+                        type2 = temp[2]
+                    if type1.upper() == 'S' or type2.upper() == 'S':
+                        no_start += 1
+                        start_state = state
+                    if no_start > 1:
+                        raise Exception('ValidationException')
+                    if type1.upper() == 'F' or type2.upper() == 'F':
+                        final_states.append(state)
+                    states.append(state)
+                if isTransitions:
+                    state1, word, state2 = [el.strip() for el in line.split(',')]
+                    transitions.append((state1, word, state2))
+                if isWords:
+                    words.append(line.strip())
+
+        if no_start == 0:
+            raise Exception('ValidationException')
+
+        for elem in transitions:
+            if elem[0] not in states or elem[2] not in states or elem[1] not in words:
+                raise Exception('ValidationException')
+
+        return True
+
 
     def accepts_input(self, input_str):
         """Return a Boolean
